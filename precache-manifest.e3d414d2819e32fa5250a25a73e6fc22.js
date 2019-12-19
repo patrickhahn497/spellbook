64439b0c44a714bc58713c844f721eb431ea69da
Patrick Hahn <patrickhahn497@gmail.com>
2019-12-19 20:38:14 +0900

    Updates

---
+++ precache-manifest.e3d414d2819e32fa5250a25a73e6fc22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117c7e08686827cbe75a5f46232a2aa4",
+    "url": "/spellbook/index.html"
+  },
+  {
+    "revision": "67c3a6cca67c42f432df",
+    "url": "/spellbook/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0ac781b6baa14bf351c4",
+    "url": "/spellbook/static/css/main.bc4384b8.chunk.css"
+  },
+  {
+    "revision": "67c3a6cca67c42f432df",
+    "url": "/spellbook/static/js/2.3e9d35cc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spellbook/static/js/2.3e9d35cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ac781b6baa14bf351c4",
+    "url": "/spellbook/static/js/main.9ab24330.chunk.js"
+  },
+  {
+    "revision": "8ffa0057f490ce4b48b9",
+    "url": "/spellbook/static/js/runtime-main.b11208ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spellbook/static/media/logo.5d5d9eef.svg"
+  }
+]);
